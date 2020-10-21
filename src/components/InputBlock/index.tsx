a2460e247992f74a28560c9de04d4d9b927bd152
lfnandoo <devfernandoof@gmail.com>
2020-10-21 15:40:15 -0300

    feat: get input value

--- src/components/InputBlock/index.tsx
+++ src/components/InputBlock/index.tsx
@@ -6,13 +6,26 @@ interface InputBlockProps {
   label: string;
   placeholder: string;
   type: string;
+  required: boolean;
+  setState: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const InputBlock: React.FC<InputBlockProps> = ({ label, ...rest }) => {
+const InputBlock: React.FC<InputBlockProps> = ({
+  label,
+  setState,
+  ...rest
+}) => {
+  const handleInputValue = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setState(e.target.value);
+    },
+    [setState],
+  );
+
   return (
     <Styles.Container>
-      <Styles.Label>{label}</Styles.Label>
-      <Styles.Input {...rest} />
+      <Styles.Label htmlFor={label}>{label}</Styles.Label>
+      <Styles.Input onChange={handleInputValue} id={label} {...rest} />
     </Styles.Container>
   );
 };
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -7,14 +7,36 @@ import logoImg from '../../assets/logo.svg';
 import * as Styles from './styles';
 
 const SignIn: React.FC = () => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = React.useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+    },
+    [],
+  );
+
   return (
     <Styles.Container>
       <Styles.Main>
         <img src={logoImg} alt="Nave.rs" />
-        <Styles.Form>
-          <InputBlock placeholder="E-mail" type="email" label="E-mail" />
-          <InputBlock placeholder="Senha" type="password" label="Senha" />
-          <Styles.Button>Entrar</Styles.Button>
+        <Styles.Form onSubmit={handleSubmit}>
+          <InputBlock
+            placeholder="E-mail"
+            type="email"
+            label="E-mail"
+            required
+            setState={setPassword}
+          />
+          <InputBlock
+            placeholder="Senha"
+            type="password"
+            label="Senha"
+            required
+            setState={setEmail}
+          />
+          <Styles.Button type="submit">Entrar</Styles.Button>
         </Styles.Form>
       </Styles.Main>
     </Styles.Container>
